a89d43765bba98f516917b16235c01866e4aa2ab
Adam MILED <adam.miled53@gmail.com>
2022-09-25 22:32:48 +0100

    final commit

---
+++ android/app/src/main/kotlin/com/example/bd_voice_recog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bd_voice_recog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:highlight_text/highlight_text.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+
+      ),
+      home: SpeechScreen()
+    );
+  }
+}
+
+class SpeechScreen extends StatefulWidget {
+final Map<String,HighlightedWord> _highlights={} ;
+
+}
